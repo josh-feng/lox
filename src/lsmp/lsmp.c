fa041845c20853a2dd03a6b579cc61233db4816b
Josh Feng <joshuafeng2@gmail.com>
2022-09-05 02:01:45 -0400

    fix stack number

--- src/lsmp/lsmp.c
+++ src/lsmp/lsmp.c
@@ -1,4 +1,7 @@
-// lua simple markup(x/html) parser
+/* lua simple/sloppy markup(x/html) parser
+The first part is solely SML parser, it can be used without lua
+The second part is for lua module, and shows a usage example of the first part
+*/
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
@@ -53,36 +56,53 @@
 //
 // NB: https://github.com/lunarmodules/luaexpat/blob/master/src/lxplib.c
 
-enum MPState {  // state machin
-  MPSpre,       // initialized
-  MPSok,        // state while parsing
-  MPSfinished,  // state after finished parsing
-  MPSerror,
-  MPSstring     // state while reading a string
-};
-
 SML_Parser SML_ParserCreate (void *opt) {
   SML_Parser p = (SML_Parser) malloc(sizeof(struct SML_ParserStruct));
+  p->buf = NULL;
   return p;
 }
 
-/*
-SML_ParserCreate
-SML_SetCharDataHdlr
-SML_SetCommentHdlr
-SML_SetElementHdlr
-SML_SetUserData
-SML_Parse
-SML_ErrorString
-SML_ParserFree
-*/
+void SML_SetEncoding (SML_Parser p, const char *coding) {
+  /* TODO */
+}
 
-/*************** lua library related ***************/
+const char *SML_ErrorString (SML_Parser p) {
+  /* TODO */
+  return NULL;
+}
+
+/* the smp (sloppy markup parser) */
+enum MPState SML_Parse (SML_Parser p, const char *s, int len, int fEnd) {
+  char *c = (char *) s;
+  while (len--) {
+    switch (*c) { /* utf8 */
+      case '<': break;
+      case '>': break;
+    }
+    if (1) p->fs(p->ud, NULL, NULL);
+    if (1) p->fe(p->ud, NULL);
+    if (1) p->fd(p->ud, NULL, 0);
+    if (1) p->fc(p->ud, NULL, 0);
+    c++;
+  }
+  /* realloc p->buf */
+  return MPSok;
+}
+
+void SML_ParserFree (SML_Parser p) {
+  free(p->buf);
+  p->buf = NULL;
+  free(p);
+}
+
+/***************************************************************/
+/********************* lua library related *********************/
+/***************************************************************/
 
 #include "lua.h"
 #include "lauxlib.h"
 
-#if (LUA_VERSION_NUM >= 540)
+#if (LUA_VERSION_NUM > 503)
 #define lua_newuserdata(L, u)    lua_newuserdatauv(L, u, 1)
 #define lua_setuservalue(L, i)   lua_setiuservalue(L, i, 1)
 #define lua_getuservalue(L, i)   lua_getiuservalue(L, i, 1)
@@ -90,84 +110,61 @@ SML_ParserFree
 
 
 #if !defined(lua_pushliteral)
-#define  lua_pushliteral(L, s)	\
-	lua_pushstring(L, "" s, (sizeof(s)/sizeof(char))-1)
+#define  lua_pushliteral(L, s)  \
+  lua_pushstring(L, "" s, (sizeof(s)/sizeof(char))-1)
 #endif
 
-#define ParserType	"MarkupParser"
+#define ParserType  "MarkupParser"
 
-/* lua full userdata */
 typedef struct lsmp_userdata {
   lua_State *L;
-  SML_Parser parser;    /* associated lsmp */
+  SML_Parser parser;    /* associated sml p */
   int errorref;         /* reference to error message */
   enum MPState state;
-  luaL_Buffer *b;       /* to concatenate sequences of cdata pieces */
-  int fNoBuffer;        /* whether to buffer cdata pieces */
-  int mode; /* TODO */
 } lsmp_ud;
 
-// Auxiliary function to call a Lua handle
+/* Auxiliary function to call a Lua handle */
 static void docall (lsmp_ud *mpu, int nargs, int nres) {
   lua_State *L = mpu->L;
   assert(mpu->state == MPSok);
-  if (lua_pcall(L, nargs + 1, nres, 0) != 0) {
+  if (lua_pcall(L, nargs, nres, 0) != 0) {
     mpu->state = MPSerror;
     mpu->errorref = luaL_ref(L, LUA_REGISTRYINDEX);  /* error message */
   }
 }
 
-/* Check whether there is pending Cdata, and call its handle if necessary */
-static void dischargestring (lsmp_ud *mpu) {
-  assert(mpu->state == MPSstring);
-  mpu->state = MPSok;
-  luaL_pushresult(mpu->b);
-  docall(mpu, 1, 0);
-}
-
-// Check whether there is a Lua handle for a given event: If so,
-// put it on the stack (to be called later), and also push `self'
+/*
+Check whether there is a Lua handle for a given event: If so,
+put it on the stack (to be called later), and also push `self'
+*/
 static int getHandle (lsmp_ud *mpu, const char *handle) {
   lua_State *L = mpu->L;
-  if (mpu->state == MPSstring) dischargestring(mpu);
-  if (mpu->state == MPSerror)
-    return 0;  /* some error happened before; skip all handles */
+  if (mpu->state == MPSerror) return 0;
+  lua_getuservalue(L, 1);
   lua_pushstring(L, handle);
   lua_gettable(L, 3);
-  if (lua_toboolean(L, -1) == 0) {
-    lua_pop(L, 1);
-    return 0;
-  }
   if (!lua_isfunction(L, -1)) {
     luaL_error(L, "lxp '%s' callback is not a function", handle);
   }
-  lua_pushvalue(L, 1);  /* first argument in every call (self) */
+  lua_pushvalue(L, 1);  /* 1st arg (ud) in every call (self) */
   return 1;
 }
 
-/*********** SAX event driven ***********/
+/*********** SAX event handler ************ ud + string */
 
 void f_CharData (void *ud, const char *s, int len) {
   lsmp_ud *mpu = (lsmp_ud *) ud;
-  if (getHandle(mpu, CharDataKey)) {
-    if (mpu->state == MPSok) {
-      if (mpu->fNoBuffer == 0) {
-        mpu->state = MPSstring;
-        luaL_buffinit(mpu->L, mpu->b);
-      } else {
-        lua_pushlstring(mpu->L, s, len);
-        docall(mpu, 1, 0);
-      }
-    }
-    if (mpu->state == MPSstring) luaL_addlstring(mpu->b, s, len);
+  if (getHandle(mpu, CharDataKey) && mpu->state == MPSok) {
+    lua_pushlstring(mpu->L, s, len);
+    docall(mpu, 1 + 1, 0);
   }
 }
 
-void f_Comment (void *ud, const char *data) {
+void f_Comment (void *ud, const char *s, int len) {
   lsmp_ud *mpu = (lsmp_ud *) ud;
   if (getHandle(mpu, CommentKey)) {
-    lua_pushstring(mpu->L, data);
-    docall(mpu, 1, 0);
+    lua_pushlstring(mpu->L, s, len);
+    docall(mpu, 1 + 1, 0);
   }
 }
 
@@ -188,7 +185,8 @@ void f_StartElement (void *ud, const char *name, const char **attrs) {
         lua_settable(L, -3);
       }
     }
-    docall(mpu, 2, 0);  /* call function with self, name, and attributes */
+    /* call function with self, name, and attributes */
+    docall(mpu, 1 + 2, 0);
   }
 }
 
@@ -196,47 +194,33 @@ void f_EndElement (void *ud, const char *name) {
   lsmp_ud *mpu = (lsmp_ud *) ud;
   if (getHandle(mpu, EndElementKey)) {
     lua_pushstring(mpu->L, name);
-    docall(mpu, 1, 0);
+    docall(mpu, 1 + 1, 0);
   }
 }
 
-
 /**************** required and their assistant ****************/
 
-static lsmp_ud *checkparser (lua_State *L, int idx) {
-  lsmp_ud *mpu = (lsmp_ud *) luaL_checkudata(L, idx, ParserType);
-  luaL_argcheck(L, mpu, idx, "lsmp expected");
-  luaL_argcheck(L, mpu->parser, idx, "parser is closed");
-  return mpu;
-}
-
 static int getcallbacks (lua_State *L) {
-  checkparser(L, 1);
   lua_getuservalue(L, 1);
   return 1;
 }
 
 static int parse_aux (lua_State *L, lsmp_ud *mpu, const char *s, size_t len) {
-  luaL_Buffer b;
   mpu->L = L;
-  mpu->state = MPSok;
-  mpu->b = &b;
-  lua_settop(L, 2);
-  getcallbacks(L);
-  int status = SML_Parse(mpu->parser, s, (int)len, s == NULL);
-  if (mpu->state == MPSstring) dischargestring(mpu);
-  if (mpu->state == MPSerror) {  /* callback error? */
+  lua_settop(L, 2); /* ud + string */
+  mpu->state = SML_Parse(mpu->parser, s, (int) len, s == NULL); /* state */
+  if (mpu->state == MPSerror) {
     lua_rawgeti(L, LUA_REGISTRYINDEX, mpu->errorref);  /* get original msg. */
     lua_error(L);
   }
   if (s == NULL) mpu->state = MPSfinished;
-  if (status) {
+  if (mpu->state != MPSerror) {
     lua_settop(L, 1);  /* return parser userdata on success */
     return 1;
   }
   /* error */
   SML_Parser p = mpu->parser;
-  lua_pushnil(L); /* status */
+  lua_pushnil(L);
   lua_pushstring(L, SML_ErrorString(p));
   lua_pushinteger(L, SML_GetCurrentLineNumber(p) + 1);
   lua_pushinteger(L, SML_GetCurrentColumnNumber(p) + 1);
@@ -245,8 +229,9 @@ static int parse_aux (lua_State *L, lsmp_ud *mpu, const char *s, size_t len) {
 }
 
 static int lsmp_pos (lua_State *L) {
-  lsmp_ud *mpu = checkparser(L, 1);
+  lsmp_ud *mpu = (lsmp_ud *) luaL_checkudata(L, 1, ParserType);
   SML_Parser p = mpu->parser;
+  luaL_argcheck(L, p, 1, "parser is closed");
   lua_pushinteger(L, SML_GetCurrentLineNumber(p) + 1);
   lua_pushinteger(L, SML_GetCurrentColumnNumber(p) + 1);
   lua_pushinteger(L, SML_GetCurrentByteIndex(p) + 1);
@@ -254,10 +239,11 @@ static int lsmp_pos (lua_State *L) {
 }
 
 static int lsmp_parse (lua_State *L) {
-  lsmp_ud *mpu = checkparser(L, 1);
+  lsmp_ud *mpu = (lsmp_ud *) luaL_checkudata(L, 1, ParserType);
+  luaL_argcheck(L, mpu->parser, 1, "parser is closed");
   size_t len;
   const char *s = luaL_optlstring(L, 2, NULL, &len);
-  if (mpu->state == MPSfinished && mpu->mode == 0) {
+  if (mpu->state == MPSfinished && mpu->parser->mode == 0) {
     if (s) {
       lua_pushnil(L);
       lua_pushliteral(L, "cannot parse - document is finished");
@@ -269,13 +255,10 @@ static int lsmp_parse (lua_State *L) {
   return parse_aux(L, mpu, s, len);
 }
 
-static int lsmp_close (lua_State *L) { /* TODO */
-  /* lsmp_ud *mpu = (lsmp_ud *) luaL_testudata(L, 1, ParserType); */
+static int lsmp_close (lua_State *L) {
   lsmp_ud *mpu = (lsmp_ud *) luaL_checkudata(L, 1, ParserType);
-  luaL_argcheck(L, mpu, 1, "expat parser expected");
   int status = (mpu->state != MPSfinished) ? parse_aux(L, mpu, NULL, 0) : 1;
 
-  /* lsmpclose */
   luaL_unref(L, LUA_REGISTRYINDEX, mpu->errorref);
   mpu->errorref = LUA_REFNIL;
   if (mpu->parser) SML_ParserFree(mpu->parser);
@@ -294,23 +277,24 @@ static int lsmp_creator (lua_State *L) {
   mpu->L = L;
   mpu->state = MPSpre;
   mpu->errorref = LUA_REFNIL;
-  mpu->fNoBuffer= lua_toboolean(L, 2);
   SML_Parser p = mpu->parser = SML_ParserCreate(NULL);
   if (!p) luaL_error(L, "SML_ParserCreate failed");
 
   luaL_checktype(L, 1, LUA_TTABLE);
   lua_pushvalue(L, 1);
   lua_setuservalue(L, -2);
-  /* TODO set other parser options */
-  /*
-  lua_getfield(L, 1, "singleton");
-  lua_getfield(L, 1, "mode");
-  */
 
-  SML_SetUserData(p, mpu);
-  SML_SetCharDataHdlr(p, f_CharData);
-  SML_SetElementHdlr(p, f_StartElement, f_EndElement);
-  SML_SetCommentHdlr(p, f_Comment);
+  /* initialize sml parser */
+  p->ud = mpu;
+  lua_getfield(L, 1, "singletons");
+  p->singletons = lua_tolstring(L, -1, NULL);
+  lua_getfield(L, 1, "mode");
+  p->mode = lua_tointeger(L, -1);
+  p->r = p->c = p->i = 0;
+  p->fd = f_CharData;
+  p->fs = f_StartElement;
+  p->fe = f_EndElement;
+  p->fc = f_Comment;
   return 1;
 }
 
@@ -347,8 +331,10 @@ LUA_API int luaopen_lsmp ( lua_State *L ) {
 
   luaL_newlib(L, lsmp_funcs); /* the module table */
   luaL_newlib(L, lsmp_mt);    /* the module metatable */
-  lua_pushstring(L, "MIT (c) Josh Feng " __DATE__);
+  lua_pushstring(L, "Lua " LUA_VDIR); /* + compiling info */
   lua_setfield(L, -2, "__metatable");
   lua_setmetatable(L, -2);
+  lua_pushstring(L, "MIT License (c) " __DATE__); /* + version */
+  lua_setfield(L, -2, "lic");
   return 1;
 }
--- src/lsmp/lsmp.h
+++ src/lsmp/lsmp.h
@@ -1,52 +1,81 @@
-/* lua simple/sloppy markup(html/xml/xhtml) parser is a SAX XML parser */
+/* simple/sloppy markup(html/xml/xhtml) parser is a SAX XML parser */
 #ifndef __lsmp_h
 #define __lsmp_h
 
-/* associated lsmp (XML_ParserStruct is in libexpat's xmlparse.c) */
-struct SML_ParserStruct {
-  void *ud;
-  char *buf;
-  /* TODO */
-  const char *singletons;
-  int mode; /* byte flags */
-  unsigned int r, c; /* row and column */
-  unsigned int i;    /* byte index */
-  const char *endoding;
-};
-typedef struct SML_ParserStruct *SML_Parser;
+#define StartCdataKey     "StartCdataSection"
+#define EndCdataKey       "EndCdataSection"
+#define CharDataKey       "CharData"
+#define CommentKey        "Comment"
+#define StartElementKey   "StartElement"
+#define EndElementKey     "EndElement"
+
+#define M_strict    0x00
 
 typedef void (*SML_StartElementHdlr) (void *ud, const char *name, const char **atts);
 typedef void (*SML_EndElementHdlr)   (void *ud, const char *name);
 typedef void (*SML_CharDataHdlr)     (void *ud, const char *s, int len);
-typedef void (*SML_CommentHdlr)      (void *ud, const char *data);
+typedef void (*SML_CommentHdlr)      (void *ud, const char *s, int len);
 
-/*
-typedef void (*SML_SetUserData)     (SML_Parser, void *ud);
-typedef void (*SML_SetElementHdlr)  (SML_Parser, SML_StartElementHdlr, SML_EndElementHdlr);
-typedef void (*SML_SetCharDataHdlr) (SML_Parser, SML_CharDataHdlr);
-typedef void (*SML_SetCommentHdlr)  (SML_Parser, SML_CommentHdlr);
+typedef struct SML_ParserStruct {
+  void *ud; /* userdata */
+  char *buf;
+  unsigned int r, c; /* row and column */
+  unsigned int i;    /* byte index */
+  SML_CharDataHdlr     fd;
+  SML_StartElementHdlr fs;
+  SML_EndElementHdlr   fe;
+  SML_CommentHdlr      fc;
+  int mode;
+  const char *encoding;
+  const char *singletons; /* strstr */
+} *SML_Parser;
+
+enum MPState { /* parser status */
+  MPSpre,      /* initialized */
+  MPSok,       /* state while parsing */
+  MPSfinished, /* state after finished parsing */
+  MPSerror,
+  MPSstring    /* state while reading a string */
+};
+
+/* markup <...> / text ...
+
+  0x00 strict 
+  0x01 escape    \\ \< \> \" \' \x         
+  0x02 space     <_ _>
+  0x04 quotation dominant ".." '..'
+  0x08 
+  0x10 
+  0x20 
+  0x40 
+  0x80 
+ 
+  <..1<..2>  0x00/q  <..1<..2>
+  <..1>..2>  0x00/q  <..1> and ..2>
+
+  MPSpre -> quote -> MPSok
+         -> text  -> MPSok
+         -> tag   -> MPSok
+  MPSstring ->
+  MPSok
+  MPSerror
+  MPSfinished
 */
 
-void SML_SetUserData     (SML_Parser, void *ud);
+/*
 void SML_SetElementHdlr  (SML_Parser, SML_StartElementHdlr, SML_EndElementHdlr);
 void SML_SetCharDataHdlr (SML_Parser, SML_CharDataHdlr);
 void SML_SetCommentHdlr  (SML_Parser, SML_CommentHdlr);
+*/
 
 #define SML_GetCurrentLineNumber(p)     ((p)->r)
 #define SML_GetCurrentColumnNumber(p)   ((p)->c)
 #define SML_GetCurrentByteIndex(p)      ((p)->i)
 
-SML_Parser  SML_ParserCreate (void *);
-void        SML_SetEncoding  (SML_Parser, const char *);
-int         SML_Parse        (SML_Parser, const char *s, int len, int fEnd);
-const char *SML_ErrorString  (SML_Parser);
-void        SML_ParserFree   (SML_Parser);
-
-#define StartCdataKey             "StartCdataSection"
-#define EndCdataKey               "EndCdataSection"
-#define CharDataKey               "CharData"
-#define CommentKey                "Comment"
-#define StartElementKey           "StartElement"
-#define EndElementKey             "EndElement"
+SML_Parser    SML_ParserCreate (void *opt);
+void          SML_SetEncoding  (SML_Parser p, const char *coding);
+enum MPState  SML_Parse        (SML_Parser p, const char *s, int len, int fEnd);
+const char   *SML_ErrorString  (SML_Parser p);
+void          SML_ParserFree   (SML_Parser p);
 
 #endif
